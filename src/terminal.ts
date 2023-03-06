243debb27357f0b322ed5c67133a97eec4652c29
Yidadaa <yidadaa@qq.com>
2023-03-06 20:17:42 +0800

    feat: add escape args function

--- src/terminal.ts
+++ src/terminal.ts
@@ -9,3 +9,25 @@ export function runCommand(commandText: string) {
   globalTerminal.show();
   globalTerminal.sendText(commandText);
 }
+
+/**
+ * return a shell compatible format
+ * @param args
+ * @copyright modified from: https://github.com/xxorax/node-shell-escape/blob/master/shell-escape.js
+ * @returns
+ */
+function escapeShellArgs(args: string[]) {
+  const ret: string[] = [];
+
+  args.forEach(function (arg) {
+    if (/[^A-Za-z0-9_\/:=-]/.test(arg)) {
+      arg = "'" + arg.replace(/'/g, "'\\''") + "'";
+      arg = arg
+        .replace(/^(?:'')+/g, "") // unduplicate single-quote at the beginning
+        .replace(/\\'''/g, "\\'"); // remove non-escaped single-quote if there are enclosed between 2 escaped
+    }
+    ret.push(arg);
+  });
+
+  return ret.join(" ");
+}
